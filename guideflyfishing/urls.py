5fafd74e6011bcb5be56e2580293259ef5acf6f3
Stewart Gibbons <thor@bluestrike.co.uk>
2024-08-27 17:39:25 +0000

    BUG-fixed name of site not being in approved convention

--- guideflyfishing/urls.py
+++ guideflyfishing/urls.py
@@ -1,10 +1,10 @@
-"""Guide_Fly_Fishing URL Configuration
+"""guideflyfishing URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
-    1. Add an import:  from my_app import views
+    1. Add an import:  from my_app import viewsg
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
