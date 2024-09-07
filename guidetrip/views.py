d4d5864c3a64ccb65262caab690758235ea62717
Stewart Gibbons <thor@bluestrike.co.uk>
2024-09-07 16:47:51 +0000

    Feature-added basic views for the trips data

--- guidetrip/views.py
+++ guidetrip/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import trips
 
 # Create your views here.
+def all_trips(request):
+    """ A view to return the all trips and show sorting and search result"""
+
+    all_trips = trips.objects.all()
+    
+   # alltrips is the variable called in the html file so show the data
+    context = {
+        'alltrips': all_trips,
+    }
+
+    return render(request, 'all_trips.html', context )
+
+ 
