b073f9cc018ba2cad5cd780d46d09f552ed38a18
Stewart Gibbons <thor@bluestrike.co.uk>
2024-10-06 13:32:04 +0000

    UG-fixed problem with item quantity in bag page

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 from guidetrip.models import trips
 
@@ -18,7 +18,7 @@ def add_to_bag(request, item_id):
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
-        messages.success(request, f'Added {product.venue} to your bag')
+        messages.success(request, f'Updated {product.venue} quantity to{bag[item_id]}')
     else:
         bag[item_id] = quantity
         messages.success(request, f'Added {product.venue} to your bag')
@@ -29,24 +29,32 @@ def add_to_bag(request, item_id):
 def adjust_bag(request, item_id):
     """ Adjust the quantity of the specified product amount to the shopping bag """
 
+    product = get_object_or_404(trips, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
 
     if quantity > 0:
         bag[item_id] = quantity
+        messages.success(request, f'Updated {product.venue} quantity to {bag[item_id]}')
     else:
         bag.pop(item_id)
+        messages.success(request, f'Removed {product.venue} from your bag')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
 
 def remove_from_bag(request, item_id):
     """ remove the item completely from bag """
-    bag = request.session.get('bag', {})
+    
+    product = get_object_or_404(trips, pk=item_id)
+    bag = request.session.get('bag', {})   
+   
     try:
         bag.pop(item_id)
+        messages.success(request, f'Removed {product.venue} from your bag')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
     except Exception as e:
-         return HttpResponse(status=500)
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
