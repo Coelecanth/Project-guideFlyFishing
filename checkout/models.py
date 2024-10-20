eb1c2ca97fb9d549730b03198a16c96d26f54a01
Stewart Gibbons <thor@bluestrike.co.uk>
2024-10-20 14:47:14 +0000

    Add-Complete Stripe payment

--- checkout/models.py
+++ checkout/models.py
@@ -34,7 +34,7 @@ class Order(models.Model):
         Update grand total each time a line item is added,
         accounting for delivery costs.
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] 
         self.grand_total = self.order_total 
         self.save()
 
@@ -62,10 +62,10 @@ class OrderLineItem(models.Model):
         Override the original save method to set the lineitem total
         and update the order total.
         """
-        self.lineitem_total = self.product.price * self.quantity
+        self.lineitem_total = self.product.cost * self.quantity
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f'Venue {self.trips.venue} on order {self.order.order_number}'
+        return f'Venue {self.product.venue} on order {self.order.order_number}'
 
        
