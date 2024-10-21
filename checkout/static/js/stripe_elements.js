19fc3cd3aa67269474e24e5fdf479dcb786d7b3e
Stewart Gibbons <thor@bluestrike.co.uk>
2024-10-21 19:40:00 +0000

    Add-Webhook handler

--- checkout/static/js/stripe_elements.js
+++ checkout/static/js/stripe_elements.js
@@ -25,9 +25,7 @@ var style = {
         iconColor: '#dc3545'
     }
 };
-var card = elements.create('card', {
-    style: style
-});
+var card = elements.create('card', {style: style});
 card.mount('#card-element');
 
 // Handle realtime validation errors on the card element
@@ -49,19 +47,16 @@ card.addEventListener('change', function (event) {
 // Handle form submit
 var form = document.getElementById('payment-form');
 
-form.addEventListener('submit', function (ev) {
+form.addEventListener('submit', function(ev) {
     ev.preventDefault();
-    card.update({
-        'disabled': true
-    });
+    card.update({ 'disabled': true});
     $('#submit-button').attr('disabled', true);
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
         }
-    }).then(function (result) {
+    }).then(function(result) {
         if (result.error) {
-            // capture error and show end user in page - eg insufficient funds 
             var errorDiv = document.getElementById('card-errors');
             var html = `
                 <span class="icon" role="alert">
@@ -69,15 +64,11 @@ form.addEventListener('submit', function (ev) {
                 </span>
                 <span>${result.error.message}</span>`;
             $(errorDiv).html(html);
-            card.update({
-                'disabled': false
-            });
+            card.update({ 'disabled': false});
             $('#submit-button').attr('disabled', false);
         } else {
-            // process paymemt 
             if (result.paymentIntent.status === 'succeeded') {
                 form.submit();
-                // payment complete
             }
         }
     });
