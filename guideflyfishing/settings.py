35fbd32c611c17e7ee56377b0279490210178e57
Stewart Gibbons <thor@bluestrike.co.uk>
2024-11-03 15:08:08 +0000

    ADD-aws static deployment-3

--- guideflyfishing/settings.py
+++ guideflyfishing/settings.py
@@ -200,7 +200,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'https://guideflyfishing.s3.eu-north-1.amazonaws.com/test/'
+    AWS_STORAGE_BUCKET_NAME = 'guideflyfishing'
     AWS_S3_REGION_NAME = 'us-east-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
@@ -215,7 +215,6 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
-
 
 
 # Stripe
