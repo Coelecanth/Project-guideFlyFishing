ae5d3b9d35ec4e9afd0f6c5a33864f67b451efea
Stewart Gibbons <thor@bluestrike.co.uk>
2024-11-09 13:36:46 +0000

    ADD-function to send email for site from Gmail

--- guideflyfishing/settings.py
+++ guideflyfishing/settings.py
@@ -77,6 +77,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'guideflyfishing.urls'
 
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
@@ -106,6 +107,7 @@ TEMPLATES = [
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
+
 AUTHENTICATION_BACKENDS = (
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
@@ -114,11 +116,9 @@ AUTHENTICATION_BACKENDS = (
     'allauth.account.auth_backends.AuthenticationBackend',
 )
 
+
 SITE_ID = 1
 
-
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
@@ -199,7 +199,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # AWS config files section 
 
 if 'USE_AWS' in os.environ:
-    # Cache control -only for static content
+    # Cache control -only for static contentcrak on 
     AWS_S3_OBJECT_PARAMETERS = {
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
@@ -229,6 +229,19 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 DEFAULT_FROM_EMAIL = 'GuideFlyFishing@example.com'
+
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'gff@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 
 # Default primary key field type
