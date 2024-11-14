8015d60fd5e8d52664fc1e7fc26197371d7091d1
Stewart Gibbons <thor@bluestrike.co.uk>
2024-11-14 10:06:51 +0000

    FIX-issue with Python lint testing documentation for TESTING.md

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,13 +1,12 @@
 from django.shortcuts import get_object_or_404
 from guidetrip.models import trips
 
-def bag_contents(request):
-    
 
+def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
-    bag = request.session.get('bag',{} )
+    bag = request.session.get('bag', {})
     grand_total = 0
 
     for item_id, quantity in bag.items():
--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import (render, redirect,
-        reverse, HttpResponse, get_object_or_404)
+    reverse, HttpResponse, get_object_or_404)
 from django.contrib import messages
 from guidetrip.models import trips
 
@@ -31,7 +31,7 @@ def add_to_bag(request, item_id):
 
 
 def adjust_bag(request, item_id):
-    """ Adjust the quantity of the specified """ 
+    """ Adjust the quantity of the specified """
     """ product amount to the shopping bag """
 
     product = get_object_or_404(trips, pk=item_id)
@@ -41,11 +41,11 @@ def adjust_bag(request, item_id):
     if quantity > 0:
         bag[item_id] = quantity
         messages.success(request,
-                f'Updated {product.venue} quantity to {bag[item_id]}')
+            f'Updated {product.venue} quantity to {bag[item_id]}')
     else:
         bag.pop(item_id)
         messages.success(request,
-                f'Removed {product.venue} from your bag')
+            f'Removed {product.venue} from your bag')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
